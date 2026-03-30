a2aa9d9d340736e0247b1b1e34be9d75cc689527
duytranvan6904 <tranvanduy6904@gmail.com>
2026-03-30 14:18:46 +0700

    Convert hrc_ws to a normal folder

---
+++ hrc_ws/build/hrc_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/duy/Experiment/hrc_ws/src/hrc_bringup
+# Build directory: /home/duy/Experiment/hrc_ws/build/hrc_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
